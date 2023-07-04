3d5f108045fa42f6b22373cf95d6c41f9436ffb9
iqba78 <92272913+iqba78@users.noreply.github.com>
2023-07-04 11:00:46 +0700

    derp type

--- derp_whyred.mk
+++ derp_whyred.mk
@@ -12,6 +12,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, vendor/derp/config/common_full_phone.mk)
 
 # derp Properties
+DERP_BUILDTYPE := PREDATOR
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
